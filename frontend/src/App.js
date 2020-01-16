a64a8641a87f399c57d5712f195316d6330323a9
João Lima <jpalvesl101@gmail.com>
2020-01-16 03:35:17 -0300

    Separating app into components and accessing the api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,146 +1,46 @@
 import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevItem from './components/DevItem'
+import DevForm from './components/DevForm'
+
 function App() {
-  const [github_username, setGithub_username] = useState('')
-  const [techs, setTechs] = useState('')
-
-  const [latitude, setLatitude] = useState('')
-  const [longitude, setLongitude] = useState('')
-
-  useEffect( () => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords
-
-        setLatitude(latitude)
-        setLongitude(longitude)
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000
-      }
-    )
-  }, [] )
-
-  async function handleSubmit(event) {
-    event.preventDefault()
+  const [devs, setDevs] = useState([])
 
 
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs')
+
+      setDevs(response.data)
+    }
+    loadDevs()
+  }, [])
+
+  async function handleSubmit(data) {
+    const response = await api.post('devs', data)
+
+    setDevs([...devs, response.data])
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input 
-              name="github_username" 
-              id="github_username" 
-              required
-              value={github_username}
-              onChange={e => setGithub_username(e.target.value)}
-            />
-          </div>
-          
-
-          <div className="input-block">
-            <label htmlFor="techs">Teconlogias</label>
-            <input 
-              name="techs" 
-              id="techs" 
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input 
-                type="number" 
-                name="latitude" 
-                id="latitude" 
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input 
-                type="number" 
-                name="longitude" 
-                id="longitude" 
-                required
-                value={longitude} 
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleSubmit}/>
       </aside>
       <main>
         <ul>
-        <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/47665775?s=460&v=4" alt="João Lima"/>
-              <div className="user-info">
-                <strong>João Lima</strong>
-                <span>Python, ReactJS, Node.js</span>
-              </div>
-            </header>
-            <p>Graduando em Engenharia da Computação IFPB CG</p>
-            <a href="https://github.com/jpalvesl/">Acessar perfil no github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        
 
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/47665775?s=460&v=4" alt="João Lima"/>
-              <div className="user-info">
-                <strong>João Lima</strong>
-                <span>Python, ReactJS, Node.js</span>
-              </div>
-            </header>
-            <p>Graduando em Engenharia da Computação IFPB CG</p>
-            <a href="https://github.com/jpalvesl/">Acessar perfil no github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/47665775?s=460&v=4" alt="João Lima"/>
-              <div className="user-info">
-                <strong>João Lima</strong>
-                <span>Python, ReactJS, Node.js</span>
-              </div>
-            </header>
-            <p>Graduando em Engenharia da Computação IFPB CG</p>
-            <a href="https://github.com/jpalvesl/">Acessar perfil no github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/47665775?s=460&v=4" alt="João Lima"/>
-              <div className="user-info">
-                <strong>João Lima</strong>
-                <span>Python, ReactJS, Node.js</span>
-              </div>
-            </header>
-            <p>Graduando em Engenharia da Computação IFPB CG</p>
-            <a href="https://github.com/jpalvesl/">Acessar perfil no github</a>
-          </li>
         </ul>
 
       </main>
